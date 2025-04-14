37d575d41f1bc219a91637bdec2583e4f60fe9fb
Beishebaev01 <bshv228@icloud.com>
2025-04-14 21:01:26 +0600

    adding hw2

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from posts.models import Post
 
 
 def test_view(request):
@@ -7,3 +8,8 @@ def test_view(request):
 
 def html_view(request):
     return render(request, 'base.html')
+
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/post_list.html', context={'posts': posts})
